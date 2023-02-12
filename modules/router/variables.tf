206bc73090e6903afa3e13c1f5a56e7c47a0a84e
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-02-12 23:20:41 +0100

    Create variables.tf

---
+++ modules/router/variables.tf
@@ -0,0 +1,14 @@
+variable "router_name" {
+  type        = string
+  description = "Name of the router"
+}
+
+variable "subnet_region" {
+  type        = string
+  description = "Region of the subnet"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC"
+}
