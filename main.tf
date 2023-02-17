8c92462bdf1b11e43f1eec36183802e642f05896
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-02-17 14:26:27 +0100

    Add public compute instance for front app

--- main.tf
+++ main.tf
@@ -22,3 +22,20 @@ module "projet_de_specialite_subnet_private" {
     module.projet_de_specialite_vpc
   ]
 }
+
+module "projet_de_specialite_instance_public_app" {
+  source                      = "./modules/compute"
+  instance_name               = "projet-de-specialite-instance-public-app"
+  instance_type               = "e2-medium"
+  instance_os                 = "debian-cloud/debian-11"
+  instance_ssh_key            = "gaetanlhf:${file("gaetanlhf-aible-gcp.pub")}"
+  network_name                = module.projet_de_specialite_vpc.vpc_name
+  subnet_name                 = module.projet_de_specialite_subnet_public.subnet_name
+  instance_private_ip         = "10.1.0.2"
+  instance_enable_external_ip = true
+  instance_tags               = ["projet-de-specialite-compute", "projet-de-specialite-compute-public", "projet-de-specialite-compute-public-app"]
+  depends_on = [
+    module.projet_de_specialite_vpc,
+    module.projet_de_specialite_subnet_public
+  ]
+}
