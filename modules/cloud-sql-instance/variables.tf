b0c89b33ed68c42f381a195de4d820a1818ffc29
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-03-10 16:00:46 +0100

    Update variables.tf

--- modules/cloud-sql-instance/variables.tf
+++ modules/cloud-sql-instance/variables.tf
@@ -12,3 +12,15 @@ variable "cloud_sql_instance_tier" {
   type        = string
   description = "Cloud SQL VM"
 }
+
+variable "cloud_sql_instance_enable_private" {
+  type        = bool
+  description = "Enable or disable private IP instance"
+}
+
+variable "cloud_sql_instance_vpc" {
+  type        = string
+  default     = ""
+  nullable    = true
+  description = "Private VPC name"
+}
