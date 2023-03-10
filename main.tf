997b0875fe7adb16aaf9d8fabd2d7d029a1da8d9
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-03-10 11:52:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "projet_de_specialite_instance_public_app" {
   compute_name               = "projet-de-specialite-compute-public-app"
   compute_type               = "e2-medium"
   compute_os                 = "debian-cloud/debian-11"
-  compute_ssh_key            = "gaetanlhf:${file("gaetanlhf-aible-gcp.pub")}"
+  compute_ssh_key            = "gaetanlhf:${file("ssh_keys/gaetanlhf.pub")}"
   vpc_name                   = module.projet_de_specialite_vpc.vpc_name
   subnet_name                = module.projet_de_specialite_subnet_public.subnet_name
   compute_private_ip         = "10.1.0.2"
@@ -37,5 +37,24 @@ module "projet_de_specialite_instance_public_app" {
   depends_on = [
     module.projet_de_specialite_vpc,
     module.projet_de_specialite_subnet_public
+  ]
+}
+
+# Sharonn
+
+module "projet_de_specialite_instance_private_posts" {
+  source                     = "./modules/compute"
+  compute_name               = "projet-de-specialite-compute-public-app"
+  compute_type               = "e2-medium"
+  compute_os                 = "debian-cloud/debian-11"
+  compute_ssh_key            = "\ngaetanlhf:${file("ssh_keys/gaetanlhf.pub")}\nsharonn:${file("ssh_keys/sharonn.pub")}"
+  vpc_name                   = module.projet_de_specialite_vpc.vpc_name
+  subnet_name                = module.projet_de_specialite_subnet_private.subnet_name
+  compute_private_ip         = "10.1.0.2"
+  compute_enable_external_ip = false
+  compute_tags               = ["projet-de-specialite-compute", "projet-de-specialite-compute-private", "projet-de-specialite-compute-private-posts"]
+  depends_on = [
+    module.projet_de_specialite_vpc,
+    module.projet_de_specialite_subnet_private
   ]
 }
