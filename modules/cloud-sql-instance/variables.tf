6b985c50b73e249d7ab1711b4987527aee1f342f
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-03-13 23:38:00 +0100

    Update variables.tf

--- modules/cloud-sql-instance/variables.tf
+++ modules/cloud-sql-instance/variables.tf
@@ -8,7 +8,29 @@ variable "cloud_sql_instance_version" {
   description = "The MySQL, PostgreSQL or SQL Server version to use"
 }
 
+variable "cloud_sql_instance_deletion_protection" {
+  type        = string
+  description = "Deletion protection"
+}
+
 variable "cloud_sql_instance_tier" {
   type        = string
   description = "Cloud SQL VM"
+}
+
+variable "cloud_sql_instance_enable_private" {
+  type        = bool
+  description = "Enable or disable private IP instance"
+}
+
+variable "cloud_sql_instance_vpc" {
+  type        = string
+  default     = ""
+  nullable    = true
+  description = "Private VPC name"
+}
+
+variable "cloud_sql_instance_enable_iam_authentication" {
+  type        = bool
+  description = "Enable or disable Cloud IAM authentication"
 }
