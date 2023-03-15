cab7155ab964eaf138a9fef1ef7ef40a13ca11fa
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-03-15 16:34:13 +0100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -23,16 +23,16 @@ resource "google_project_iam_binding" "projet_de_specialite_iam_cloud_sql" {
     "roles/cloudsql.client",
     "roles/iam.serviceAccountTokenCreator"
   ])
-    role = each.key
-    members = [
-      "serviceAccount:${module.projet_de_specialite_service_account_auth.service_account_email}",
-      "serviceAccount:${module.projet_de_specialite_service_account_posts.service_account_email}",
-      "serviceAccount:${module.projet_de_specialite_service_account_profile.service_account_email}",
-      "serviceAccount:${module.projet_de_specialite_service_account_comments.service_account_email}",
-      "serviceAccount:${module.projet_de_specialite_service_account_subs.service_account_email}",
-      "serviceAccount:${module.projet_de_specialite_service_account_mp.service_account_email}",
-    "user:gaetan.le.heurt.finot@etud.u-picardie.fr"]
-  
+  role = each.key
+  members = [
+    "serviceAccount:${module.projet_de_specialite_service_account_auth.service_account_email}",
+    "serviceAccount:${module.projet_de_specialite_service_account_posts.service_account_email}",
+    "serviceAccount:${module.projet_de_specialite_service_account_profile.service_account_email}",
+    "serviceAccount:${module.projet_de_specialite_service_account_comments.service_account_email}",
+    "serviceAccount:${module.projet_de_specialite_service_account_subs.service_account_email}",
+    "serviceAccount:${module.projet_de_specialite_service_account_mp.service_account_email}",
+  "user:gaetan.le.heurt.finot@etud.u-picardie.fr"]
+
 }
 
 resource "google_storage_bucket_iam_policy" "projet_de_specialite_posts_bucket_iam_policy" {
