6caac06c71e02197f179d3324a7d402c9adfd6ad
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-05-06 17:37:52 +0200

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -2,4 +2,5 @@ resource "google_compute_subnetwork" "subnet" {
   name          = var.subnet_name
   ip_cidr_range = var.subnet_ip_range
   network       = var.subnet_network_name
+  private_ip_google_access = var.subnet_private_ip_google_access
 }
