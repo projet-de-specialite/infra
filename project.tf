72a6b086bb4921a3687f9c5590904a20380e66ee
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-05-15 12:00:56 +0200

    Update project.tf

--- project.tf
+++ project.tf
@@ -1,4 +1,6 @@
 resource "google_project" "project" {
-  project_id = "projet-de-specialite-377322"
-  name       = "projet-de-specialite"
+  project_id          = "projet-de-specialite-master1"
+  name                = "projet-de-specialite"
+  auto_create_network = false
+  billing_account     = "014969-508905-EAF372"
 }
