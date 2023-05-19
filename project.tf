64586c7236ceae90108ae0fe8096a3f1d72a4157
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-05-19 11:20:29 +0200

    Update main.tf

--- project.tf
+++ project.tf
@@ -1,5 +1,9 @@
+resource "random_id" "random_id_project" {
+  byte_length = 4
+}
+
 resource "google_project" "project" {
-  project_id          = "projet-de-specialite-master1"
+  project_id          = "projet-de-specialite-${random_id.random_id_project.hex}"
   name                = "projet-de-specialite"
   auto_create_network = false
   billing_account     = "010CC9-6CBBB5-C42FC0"
